4f8f89426f9a438d5a7ebbf995f3b792153f309c
robbyardha <robbyardha@gmail.com>
2021-05-06 18:08:10 +0700

    addcontent

--- app/src/main/java/com/ardhacodes/subs1_jetpack/ui/detail/DetailMovieTvActivity.kt
+++ app/src/main/java/com/ardhacodes/subs1_jetpack/ui/detail/DetailMovieTvActivity.kt
@@ -5,20 +5,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.ardhacodes.subs1_jetpack.R
+import com.ardhacodes.subs1_jetpack.databinding.ActivityDetailMovieTvBinding
+import com.ardhacodes.subs1_jetpack.databinding.ContentDetailMovieTvBinding
 
 class DetailMovieTvActivity : AppCompatActivity() {
     companion object{
         const val EXTRA_MOV = "extra_mov"
     }
+
+    private lateinit var detailcontentsbinds : ContentDetailMovieTvBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_movie_tv)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        val activityDetailContentBinding = ActivityDetailMovieTvBinding.inflate(layoutInflater)
+        detailcontentsbinds = activityDetailContentBinding.detailContent
+
+        setContentView(activityDetailContentBinding.root)
+        setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
