fbf5c00866e0a03feabcda76e13a848a41c3976b
robbyardha <robbyardha@gmail.com>
2021-05-16 01:36:32 +0700

    change testing

---
+++ app/src/androidTest/java/com/ardhacodes/subs1_jetpack/ui/main/MainActivityInsTest.kt
@@ -0,0 +1,47 @@
+package com.ardhacodes.subs1_jetpack.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.ardhacodes.subs1_jetpack.R
+import com.ardhacodes.subs1_jetpack.utils.MoviesTvDataDummy
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import java.util.regex.Pattern.matches
+
+class MainActivityInsTest
+{
+    private val mov = MoviesTvDataDummy.DataMovies()
+    private val tv = MoviesTvDataDummy.DataTvShow()
+
+    @get:Rule
+    var activityrule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun detailMovie() {
+        onView(withId(R.id.rv_movie)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0))
+        onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        onView(withId(R.id.cardView2)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.tv_title)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.tv_genre)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.tv_duration)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.tv_year)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.tv_score)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.tv_overview)).check(ViewAssertions.matches(isDisplayed()))
+
+        onView(withId(R.id.tv_title)).check(ViewAssertions.matches(withText(mov[0].title)))
+        onView(withId(R.id.tv_overview)).check(ViewAssertions.matches(withText(mov[0].overview)))
+
+        Espresso.pressBack()
+    }
+
+}
+
--- app/src/test/java/com/ardhacodes/subs1_jetpack/ui/main/MainActivityTest.kt
+++ app/src/test/java/com/ardhacodes/subs1_jetpack/ui/main/MainActivityTest.kt
@@ -1,49 +1,76 @@
 package com.ardhacodes.subs1_jetpack.ui.main
 
 import com.ardhacodes.subs1_jetpack.ui.detail.DetailViewModel
+import com.ardhacodes.subs1_jetpack.ui.movie.MovieViewModel
+import com.ardhacodes.subs1_jetpack.ui.tv.TvViewModel
 import com.ardhacodes.subs1_jetpack.utils.MoviesTvDataDummy
+import junit.framework.TestCase
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
 class MainActivityTest
 {
-    private lateinit var viewmodel: DetailViewModel
+
+    private lateinit var viewmodeldetail: DetailViewModel
+    private lateinit var viewmodelmov: MovieViewModel
+    private lateinit var viewmodeltv: TvViewModel
     val dataMov = MoviesTvDataDummy.DataMovies()[0]
     val dataTv = MoviesTvDataDummy.DataTvShow()[0]
     val movieId = dataMov.title
     val tvShowId = dataTv.title
 
     @Before
-    fun setDataMovie() {
-        viewmodel = DetailViewModel()
-        viewmodel.setMovieById(movieId)
-        viewmodel.setTvShowById(tvShowId)
+    fun setData()
+    {
+        viewmodelmov = MovieViewModel()
+        viewmodelmov.getdDataMovie()
+
+        viewmodeltv = TvViewModel()
+        viewmodeltv.getdDataTv()
+
+
+        viewmodeldetail = DetailViewModel()
+        viewmodeldetail.setMovieById(movieId)
+        viewmodeldetail.setTvShowById(tvShowId)
     }
 
     @Test
-    fun getData() {
-        val movie = viewmodel.getMovieById()
-        val tvShow = viewmodel.getTvShowById()
-        assertNotNull(tvShow)
+    fun getListMovieItem() {
+        val movies = viewmodelmov.getdDataMovie()
+        TestCase.assertNotNull(movies)
+        assertEquals(10, movies.size)
+    }
+
+    @Test
+    fun getListTvShowItem() {
+        val tvShows = viewmodeltv.getdDataTv()
+        TestCase.assertNotNull(tvShows)
+        assertEquals(11, tvShows.size)
+    }
+
+    @Test
+    fun getNotNull()
+    {
+        val movie = viewmodeldetail.getMovieById()
+        val tvShow = viewmodeldetail.getTvShowById()
+
         assertNotNull(movie)
-        if (movie != null) {
-            assertEquals(dataMov.title, movie.title)
-            assertEquals(dataMov.genre, movie.genre)
-            assertEquals(dataMov.yearrelease, movie.yearrelease)
-            assertEquals(dataMov.poster, movie.poster)
-            assertEquals(dataMov.score, movie.score)
-            assertEquals(dataMov.duration, movie.duration)
-            assertEquals(dataMov.overview, movie.overview)
-        }
-        if (tvShow != null) {
-            assertEquals(dataTv.title, tvShow.title)
-            assertEquals(dataTv.genre, tvShow.genre)
-            assertEquals(dataTv.yearrelease, tvShow.yearrelease)
-            assertEquals(dataTv.poster, tvShow.poster)
-            assertEquals(dataTv.score, tvShow.score)
-            assertEquals(dataTv.duration, tvShow.duration)
-            assertEquals(dataTv.overview, tvShow.overview)
-        }
+        assertEquals(dataMov.title, movie?.title)
+        assertEquals(dataMov.genre, movie?.genre)
+        assertEquals(dataMov.yearrelease, movie?.yearrelease)
+        assertEquals(dataMov.poster, movie?.poster)
+        assertEquals(dataMov.score, movie?.score)
+        assertEquals(dataMov.duration, movie?.duration)
+        assertEquals(dataMov.overview, movie?.overview)
+
+        assertNotNull(tvShow)
+        assertEquals(dataTv.title, tvShow?.title)
+        assertEquals(dataTv.genre, tvShow?.genre)
+        assertEquals(dataTv.yearrelease, tvShow?.yearrelease)
+        assertEquals(dataTv.poster, tvShow?.poster)
+        assertEquals(dataTv.score, tvShow?.score)
+        assertEquals(dataTv.duration, tvShow?.duration)
+        assertEquals(dataTv.overview, tvShow?.overview)
     }
 }
