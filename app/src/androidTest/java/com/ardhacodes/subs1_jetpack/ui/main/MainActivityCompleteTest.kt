8d37c2212de9046688b1f5a69a7bee260fc1b8c5
robbyardha <robbyardha@gmail.com>
2021-05-18 16:51:20 +0700

    fix complete test

--- app/src/androidTest/java/com/ardhacodes/subs1_jetpack/ui/main/MainActivityCompleteTest.kt
+++ app/src/androidTest/java/com/ardhacodes/subs1_jetpack/ui/main/MainActivityCompleteTest.kt
@@ -3,6 +3,7 @@ package com.ardhacodes.subs1_jetpack.ui.main
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -44,11 +45,11 @@ class MainActivityCompleteTest
         onView(withId(R.id.tv_genre)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_genre)).check(matches(withText(Mov[0].genre)))
         onView(withId(R.id.tv_duration)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_duration)).check(matches(withText(Mov[0].duration)))
+        onView(withId(R.id.tv_duration)).check(matches(withText("Duration : ${Mov[0].duration}")))
         onView(withId(R.id.tv_year)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_year)).check(matches(withText(Mov[0].yearrelease)))
+        onView(withId(R.id.tv_year)).check(matches(withText("Release : ${Mov[0].yearrelease}")))
         onView(withId(R.id.tv_score)).check(matches(isDisplayed()))
-        onView(withId(R.id.tv_score)).check(matches(withText(Mov[0].score)))
+        onView(withId(R.id.tv_score)).check(matches(withText("Score : ${Mov[0].score}")))
         onView(withId(R.id.tv_overview)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_overview)).check(matches(withText(Mov[0].overview)))
     }
@@ -57,15 +58,18 @@ class MainActivityCompleteTest
     @Test
     fun loadDetailTvShow() {
         onView(withText("TV Show")).perform(click())
-//        onView(withId(R.id.rv_tv_show)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
-//        onView(withId(R.id.tv_title)).check(matches(isDisplayed()))
-//        onView(withId(R.id.tv_title)).check(matches(withText(dummyTvMovie[0].title)))
-//        onView(withId(R.id.tv_genre)).check(matches(isDisplayed()))
-//        onView(withId(R.id.tv_genre)).check(matches(withText(dummyTvMovie[0].genre)))
-//        onView(withId(R.id.tv_overview)).check(matches(isDisplayed()))
-//        onView(withId(R.id.tv_overview)).check(matches(withText(dummyTvMovie[0].overview)))
-//        onView(withId(R.id.tv_release)).check(matches(isDisplayed()))
-//        onView(withId(R.id.tv_release)).check(matches(withText(dummyTvMovie[0].release)))
+        onView(withId(R.id.rv_tv)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+        onView(withId(R.id.tv_title)).check(matches(withText(TvShow[0].title)))
+        onView(withId(R.id.tv_genre)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_genre)).check(matches(withText(TvShow[0].genre)))
+        onView(withId(R.id.tv_duration)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_duration)).check(matches(withText("Duration : ${TvShow[0].duration}")))
+        onView(withId(R.id.tv_year)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_year)).check(matches(withText("Release : ${TvShow[0].yearrelease}")))
+        onView(withId(R.id.tv_score)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_score)).check(matches(withText("Score : ${TvShow[0].score}")))
+        onView(withId(R.id.tv_overview)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_overview)).check(matches(withText(TvShow[0].overview)))
 //    }
     }
 }
